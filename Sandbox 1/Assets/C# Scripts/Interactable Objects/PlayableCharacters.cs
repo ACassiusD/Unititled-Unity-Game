08ca070205b9fa85c559ef63f8f4d59420d193e7
Alex Donnelly <alexandercassius@hotmail.com>
2019-12-29 18:31:04 -0500

    Random movement for unmounted mounts
    Adds some life to the game

--- Sandbox 1/Assets/C# Scripts/Interactable Objects/PlayableCharacters.cs
+++ Sandbox 1/Assets/C# Scripts/Interactable Objects/PlayableCharacters.cs
@@ -15,10 +15,28 @@ public class PlayableCharacters : MonoBehaviour {
 	public int maxJumps = 2;
     public int rotationSpeed = 3;
     public bool isBeingControlled = false;
-    
+
+    //Should be moved to a different class
+    public bool mountAi = false;
+    Random random;
+    public bool isTurning = false;
+    public bool isWalking = false;
+    public float turnLength = 0.0f;
+    public float walkLength = 0.0f;
+    public int turnDirection = 0;
+    float halfSecondLength = 0.5f;
+    public float aiRotationSpeed = 1.5f;
+    public int aiMoveSpeed = 30;
+
+
     void Start () {
         onCreate();
+        random = new Random();
         controller = GetComponent<CharacterController>();
+        if (!isBeingControlled)
+        {
+            mountAi = true;
+        }
     }
 
     //Overloadable function that is called when a mount is initialized 
@@ -30,7 +48,7 @@ public class PlayableCharacters : MonoBehaviour {
     //Update is called once per frame
     void Update()
     {
-            moveCharacterController();
+         moveCharacterController();
     }
 
 	//Control player with character controller
@@ -40,6 +58,10 @@ public class PlayableCharacters : MonoBehaviour {
         if (isBeingControlled)
         {
             getPlayerInput();
+        }
+        else if(mountAi) //AI Controlling the movement controller Should be moved to a different class
+        {
+            getMountAiInput();
         }
         else
         {
@@ -89,5 +111,70 @@ public class PlayableCharacters : MonoBehaviour {
 
             //Apply player rotation 
             transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed, 0);
+    }
+
+    void getMountAiInput()
+    {
+        moveDirection = new Vector3(0, 0, 0);
+
+        //Generate random value to see if the mount should turn or walk forward
+        if (!isTurning)
+        {
+            float max = int.MaxValue;
+            float randnum = Random.Range(0f, max);
+            double percentage = System.Math.Round((((double)randnum / (double)max) * 100), 1);
+
+            if (percentage > 98d)
+            {
+                isTurning = true;
+                turnDirection = Random.Range(-1, 1);
+                turnLength = halfSecondLength * Random.Range(0, 3);
+            }
+        }
+        else
+        {
+            turnLength -= Time.deltaTime;
+            if (turnLength < 0)
+            {
+                turnDirection = 0;
+                turnLength = 0;
+                isTurning = false;
+            }
+        }
+
+        transform.Rotate(0, turnDirection * aiRotationSpeed, 0);
+
+        //Do the same for walking
+        if (!isWalking)
+        {
+            int max = int.MaxValue;
+            int randnum = Random.Range(0, max);
+            double percentage = System.Math.Round((((double)randnum / (double)max) * 100), 1);
+
+            if (percentage > 99.5d)
+            {
+                isWalking = true;
+                walkLength = halfSecondLength * Random.Range(0, 4);
+            }
+        }
+        else
+        {
+            walkLength -= Time.deltaTime;
+            if (walkLength < 0)
+            {
+                walkLength = 0;
+                isWalking = false;
+            }
+        }
+
+        if (isTurning)
+        {
+            transform.Rotate(0, turnDirection * aiRotationSpeed, 0);
+        }
+
+        if (isWalking)
+        {
+            moveDirection = new Vector3(0, moveDirection.y, 1 * aiMoveSpeed);
+        }
     }
 }
