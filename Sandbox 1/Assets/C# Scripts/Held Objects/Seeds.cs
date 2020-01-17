bb87863a72adf13f42c909b91fcc67bb1e771c8b
Alex Donnelly <alexandercassius@hotmail.com>
2020-01-16 22:08:06 -0500

    On version 2017 LTS

---
+++ Sandbox 1/Assets/C# Scripts/Held Objects/Seeds.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seeds : CollectableItem
+{
+
+    public Seeds(string name, int qty) : base(name, qty)
+    {
+    }
+
+    void returnTest()
+    {
+        Debug.Log("return test");
+    }
+}
