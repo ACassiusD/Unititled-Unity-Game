1acdcfd4907c0d62a2f085d75b3d9587d44d805e
Alex Donnelly <alexandercassius@hotmail.com>
2023-02-21 15:10:33 -0500

    Organized project structure.

--- Sandbox 1/Assets/Scripts/Inventory/Item/InventoryItemData.cs
+++ Sandbox 1/Assets/Scripts/Inventory/Item/InventoryItemData.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /**
@@ -15,5 +13,4 @@ public class InventoryItemData : ScriptableObject
     public Sprite icon;
     public int maxStackSize;
     public int goldValue;
-
 }
