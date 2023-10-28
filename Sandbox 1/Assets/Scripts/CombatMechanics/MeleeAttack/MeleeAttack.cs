d41cd500eb2bf29fd9150ee170b36d6c482546f6
Alex Donnelly <alexandercassius@hotmail.com>
2023-10-27 22:37:50 -0400

    Using hitregistry on melee attacks to keep track of already struck enemies. Preventing enemies with multple hitboxes from being hit mutliple times, but still allowing multiple seperate entities to be hit. Using CombatComponent for Enemy Identification

--- Sandbox 1/Assets/Scripts/CombatMechanics/MeleeAttack/MeleeAttack.cs
+++ Sandbox 1/Assets/Scripts/CombatMechanics/MeleeAttack/MeleeAttack.cs
@@ -1,16 +1,34 @@
 // The MeleeAttack class represents a short-duration, close-range attack.
 // The attack's hitbox is enabled temporarily, and the activation and deactivation are managed by the owning Weapon.
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class MeleeAttack : Attack
 {
+    private Dictionary<GameObject, float> hitRegistry;
+    private float attackDuration = 0.5f; // or however long the attack lasts
     public VisualEffect meleeAnimation;
+
+    private void OnEnable()
+    {
+        hitRegistry = new Dictionary<GameObject, float>();
+    }
 
     protected override void HandleHit(Collider other)
     {
-        base.HandleHit(other);
-        SpecificAttackBehaviour();
+        CombatComponent enemyCombatComponent = other.GetComponentInParent<CombatComponent>();
+        if (enemyCombatComponent == null) return;  // Hit something that isn't an enemy
+
+        GameObject enemyRoot = enemyCombatComponent.gameObject;
+
+        if (!hitRegistry.ContainsKey(enemyRoot))
+        {
+            // This is a new enemy that we've not hit before during this attack
+            base.HandleHit(other);
+            SpecificAttackBehaviour();
+            hitRegistry.Add(enemyRoot, Time.time);
+        }
     }
 
     public override void SpecificAttackBehaviour()
